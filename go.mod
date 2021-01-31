3566f4dbadccdd6084c2fb92b21a9a433147e69d
Kieran Flaherty <kieranflaherty1995@gmail.com>
2021-01-31 20:40:46 +0000

    feat: implement /pin/{pin}/toggle endpoint to toggle pin on/off

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/kieran-flaherty/go-gpio-webapi
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.7.0
+	github.com/stianeikeland/go-rpio/v4 v4.4.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/stianeikeland/go-rpio/v4"
+)
 
 func main() {
-	fmt.Printf("Hello golang!\n")
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", rootHandler)
+	router.HandleFunc("/pin/{pin:[0-9]+}/toggle", togglePinHandler)
+
+	log.Fatal(http.ListenAndServe(":8081", cors.Handler(router)))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, raspberry!")
+}
+
+func togglePinHandler(w http.ResponseWriter, r *http.Request) {
+	requestVars := mux.Vars(r)
+	pinNum, err := strconv.Atoi(requestVars["pin"])
+
+	if err != nil {
+		http.Error(w, "Could not parse pin number", http.StatusBadRequest)
+		return
+	}
+
+	rpio.Open()
+	log.Printf("Toggling pin %d", pinNum)
+	pin := rpio.Pin(pinNum)
+	pin.Output()
+	pin.Toggle()
+	rpio.Close()
+
+	fmt.Fprintf(w, "Toggle pin %d", pinNum)
 }
